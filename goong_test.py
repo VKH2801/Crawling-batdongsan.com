b41db473d05803076cf9f24c0e601f47c214bf56
Hoàng Vũ <81975442+VKH2801@users.noreply.github.com>
2023-05-11 15:15:01 +0700

    Adding Average Price with Longitude Latitude

--- goong_test.py
+++ goong_test.py
@@ -1,17 +1,23 @@
-# import requests
+import requests
 import json
-# domain_URL = 'https://rsapi.goong.io/geocode?address='
+domain_URL = 'https://rsapi.goong.io/geocode?address='
 
 
-# API_key = '&api_key=22DfeaveAqw3AHJSXNFVYt4g3lUTlJ91ebcZt18B'
+API_key = '&api_key=22DfeaveAqw3AHJSXNFVYt4g3lUTlJ91ebcZt18B'
 
-# address = '1 Tôn Thất Thuyết, Phường 1, Quận 4, Thành phố Hồ Chí Minh, Vietnam'
+address = '1 Tôn Thất Thuyết, Phường 1, Quận 4, Thành phố Hồ Chí Minh, Vietnam'
 
-# res = requests.get(domain_URL + address + API_key)
+res = requests.get(domain_URL + address + API_key)
 
-# print(res.text)
+print(res.text)
 
-with open('temp_data.json', 'r' , encoding='utf-8') as f:
+with open('list_properties_copy.json', 'r' , encoding='utf-8') as f:
     data = json.load(f)
 
-print(range(len(data)))
+# print(range(len(data)))
+
+
+char_dot = '·'
+# for index in range(len(data)):
+#     location = data[index]['location']
+#     if ():
--- taking_average_value.py
+++ taking_average_value.py
@@ -1,24 +1,100 @@
 import json
+import requests
 
+
+domain_URL = 'https://rsapi.goong.io/geocode?address='
+
+API_key = '&api_key=22DfeaveAqw3AHJSXNFVYt4g3lUTlJ91ebcZt18B'
 
 with open('list_properties_copy.json', 'r', encoding='utf-8') as f:
     data = json.load(f)
 
 list_properties_average = []
 list_locations = []
-list_locations_results = []
+location_average = {}
+list_locations_average = []
+list_results = []
+count = 0
+for i in data:
+    location = i['location']
+    longitude = i['longitude']
+    latitude = i['latitude']
+    price = i['price']
 
-for index in data:
-    location = index['location']
-    if '.' in location:
-        location = location.replace('.', '')
+    # Chuẩn hóa kiểu dữ liệu 'giá' trong data.
+    if isinstance(price, str) and 'triệu' in price:
+        price = float(price.replace(' triệu', ''))
+
+    if isinstance(price, str) and 'nghìn' in price:
+        price = float(price.replace(' nghìn', ''))
+        price = price / 1000
+
+    if isinstance(price, str) and 'tỷ' in price:
+        price = float(price.replace(' tỷ', '').replace(',', '.'))
+        price = price * 1000
+
+    if isinstance(price, str) and 'Giá thỏa thuận' in price:
+        price = 1
+    else:
+        pass
+
+
+    # Chuẩn hóa dữ liệu location - xóa dấu '·' và add vào list_locations.
+    if '·' in location:
+        location = location.replace('·', '')
+        result = {
+            'price': price,
+            'acreage': i['acreage'],
+            'location': location,
+            'coordinates': [
+                longitude, latitude
+            ]
+        }
         list_locations.append(location)
-    
 
-for index in list_locations:
-    if index not in list_locations_results:
-        list_locations_results.append(index)
+    if location in location_average:
+        location_average[location]['sum'] += price
+        location_average[location]['count'] += 1
+        
+    else:
+        location_average[location] = {"sum": price, "count": 1,}
+        
 
 
-    
-print(list_locations)
+
+for location, value in location_average.items():
+    location_average = value["sum"] / value['count']
+    result = {
+        "location": location,
+        "average_price": location_average
+    }
+    list_properties_average.append(result)
+
+
+for index in list_properties_average:
+    address = index["location"]
+    res = requests.get(domain_URL + address + API_key)
+
+    data = json.loads(res.text)
+
+    # Lấy lat và lng
+    lat = data['results'][0]['geometry']['location']['lat']
+    lng = data['results'][0]['geometry']['location']['lng']
+
+    result = {
+        "location": location,
+        "average_price": location_average,
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                lng, lat
+            ]
+        }
+    }
+    list_results.append(result)
+
+with open('list_properties_average.json', 'w', encoding='utf-8') as f:
+    json.dump(list_results, f, ensure_ascii=False)
+
+
+# print(list_properties_average)
