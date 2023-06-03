f16ea2a9087ae0fd4be16aa9d70ade4308ea44ce
VKH2801 <20521352@gm.uit.edu.vn>
2023-06-03 09:10:14 +0700

    Adding list_result_part1.json

--- format_data.py
+++ format_data.py
@@ -1,25 +1,21 @@
-from geopy.geocoders import Nominatim
 import json
-app_name = 'format_data.py'
 
 
-# Khởi tạo geolocator
-geolocator = Nominatim(user_agent= app_name)
 
-# Các quận trong TP. Hồ Chí Minh
-districts = ["Quận 1", "Quận 2", "Quận 3", "Quận 4", "Quận 5", "Quận 6", "Quận 7", "Quận 8", "Quận 9", "Quận 10", "Quận 11", "Quận 12", "Bình Tân", "Bình Thạnh", "Gò Vấp", "Phú Nhuận", "Tân Bình", "Tân Phú", "Thủ Đức"]
+################################################################
+# Formatted -> delete data address if exists key word 'Đ.'
+################################################################
+# with open('list_results_long_latitude.json', 'r' , encoding='utf-8') as f:
+#     data = json.load(f)
 
-# Tạo từ điển lưu tọa độ của các quận
-district_coords = {}
+# list_results_long_latitude = []
 
-for district in districts:
-    # Lấy địa chỉ của quận
-    location = geolocator.geocode(district + ", Hồ Chí Minh")
-    
-    # Lưu tọa độ của quận vào từ điển
-    district_coords[district] = (location.longitude, location.latitude)
+# for i in range(len(data)):
+#     address = data[i]['address']
+#     if ('Đ.' not in address):
+#         list_results_long_latitude.append(data[i])
 
-print(district_coords)
+# with open('list_results_log_lat_formatted.json', 'w' , encoding='utf-8') as f:
+#     json.dump(list_results_long_latitude, f, ensure_ascii=False)
 
-with open('list_district_coordinates.json', 'w', encoding='utf-8') as outfile:
-    json.dump(district_coords, outfile, ensure_ascii=False)
+
--- goong_test.py
+++ goong_test.py
@@ -11,25 +11,25 @@ API_key = '&api_key=22DfeaveAqw3AHJSXNFVYt4g3lUTlJ91ebcZt18B'
 # print(res.text)
 
 list_properties_part1 = []
+list_properties_part2 = []
 
 
-with open('list_results_long_latitude.json', 'r' , encoding='utf-8') as f:
+with open('list_results_log_lat_formatted.json', 'r' , encoding='utf-8') as f:
     data = json.load(f)
+
 
 print(range(len(data)))
 
-
-
-for i in range(1, 901):
+for i in range(0, 901):
     address = data[i]["address"]
     res = requests.get(domain_URL + address + API_key)
     
 
-    data = json.loads(res.text)
+    data_res = json.loads(res.text)
 
     # Lấy lat và lng
-    lat = data['results'][0]['geometry']['location']['lat']
-    lng = data['results'][0]['geometry']['location']['lng']
+    lat = data_res['results'][0]['geometry']['location']['lat']
+    lng = data_res['results'][0]['geometry']['location']['lng']
 
     result = {
         'price': data[i]['price'],
@@ -38,32 +38,37 @@ for i in range(1, 901):
         "geometry": {
             "type": "Point",
             "coordinates": [
-                lng, lat
+                str(lng),
+                str(lat)
             ]
         }
     }
+    list_properties_part1.append(result)
     
+with open('list_results_part1.json', 'w', encoding='utf-8') as f:
+    json.dump(list_properties_part1, f, ensure_ascii=False)
 
+# for i in range(902, 1000): 
+#     address = data[i]["address"]
+#     res = requests.get(domain_URL + address + API_key)
 
-for i in range(902, 1000): 
-    address = data[i]["address"]
-    res = requests.get(domain_URL + address + API_key)
+#     data = json.loads(res.text)
 
-    data = json.loads(res.text)
+#     # Lấy lat và lng
+#     lat = data['results'][0]['geometry']['location']['lat']
+#     lng = data['results'][0]['geometry']['location']['lng']
 
-    # Lấy lat và lng
-    lat = data['results'][0]['geometry']['location']['lat']
-    lng = data['results'][0]['geometry']['location']['lng']
-
-    result = {
-        'price': data[i]['price'],
-        'size': data[i]['size'],
-        'attributes': data[i]['attribute'],
-        "geometry": {
-            "type": "Point",
-            "coordinates": [
-                lng, lat
-            ]
-        }
-    }
+#     result = {
+#         'price': data[i]['price'],
+#         'size': data[i]['size'],
+#         'attributes': data[i]['attribute'],
+#         "geometry": {
+#             "type": "Point",
+#             "coordinates": [
+#                 str(lng), 
+#                 str(lat)
+#             ]
+#         }
+#     }
+#     list_properties_part2 = append(result)
 
